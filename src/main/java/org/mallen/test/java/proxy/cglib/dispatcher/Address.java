9338df2864e675f3f034f7dfc3c390ebed997da5
mallen huang <mallen.huang@timanetworks.com>
2019-02-21 15:00:10 +0800

    add cglib DispatcherTest

---
+++ src/main/java/org/mallen/test/java/proxy/cglib/dispatcher/Address.java
@@ -0,0 +1,17 @@
+package org.mallen.test.java.proxy.cglib.dispatcher;
+
+/**
+ * @author mallen
+ * @date 2/21/19
+ */
+public class Address {
+    private String detail;
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
